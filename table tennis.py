0da227d829194a0ba28c84f830ad6f3fa9b5ec96
BruceWayne984 <73269123+BruceWayne984@users.noreply.github.com>
2020-10-22 10:39:01 +0530

    Update and rename table tennis to table tennis.py

--- table tennis.py
+++ table tennis.py
@@ -1,11 +1,11 @@
 from time_travel import machine
 t=2020
-dest="intythyr"
+dest=1985
 def newTime():
-    return t-946080000
+    return "to" and t-946080000
 while True:
     time.trvl(t-1)
     t-=1
     music.play(Huey Lewis and The News)
-    td=20460
+    td=20460s
 newtime()
